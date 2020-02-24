a94fde3638ccd6ebd67299067678ef4415b9c9f7
apqlzm <artur-czyzewski@protonmail.com>
2020-02-24 23:27:20 +0100

    update kampus scraper

--- scrapers/kampus_radio.py
+++ scrapers/kampus_radio.py
@@ -1,5 +1,5 @@
-"""
-Extract songs from https://radiokampus.fm/playlista.php
+"""Download playlist from Radio Kampus using it's api https://api.radiokampus.fm/playlista
+
 """
 
 import re
@@ -10,37 +10,35 @@ from bs4 import BeautifulSoup
 from scrapers.models import SpotifyTrack
 
 
-def __download_html(url):
-    result = requests.get(url)
-    return result.content
+def __download_json(url):
+    headers = {
+        "Origin": "https://radiokampus.fm",
+        "Referer": "https://radiokampus.fm/playlista/",
+    }
+    result = requests.get(url, headers=headers)
+    return result.json()
 
 
-def __prepare_list_of_tracks(html):
+def __prepare_list_of_tracks(json_data):
     tracks = []
-    soup = BeautifulSoup(html, "lxml")
-    div_with_table = soup.find("div", "art_view_full view_full")
-    rows = div_with_table.find_all("tr")
-    for row in rows:
-        tds = row.find_all("td")
-        if tds[1].b:
-            artist = tds[1].b.text
-            artist_track = tds[1].text
-            track_name = artist_track.replace(artist, "")
-            track_name = track_name.strip()[1:]
-            # sometimes there is a text in braces with feat. artists
-            # finding such tracks is impossible
-            track_name = re.sub(r"\(.*?\)", "", track_name).strip()
-            tracks.append(SpotifyTrack(name=track_name, artist=artist))
+    for song_data in json_data:
+        artist = song_data["artist"]
+        track_name = song_data["title"]
+        tracks.append(SpotifyTrack(name=track_name, artist=artist))
     return tracks
 
 
 def tracks(url):
-    r = __download_html(url)
+    """Downloads list of tracks
+    Args:
+        url: api url for example - https://api.radiokampus.fm/playlista/?day=20200223
+    """
+    r = __download_json(url)
     tracks = __prepare_list_of_tracks(r)
     return tracks
 
 
 if __name__ == "__main__":
-    r = __download_html("https://radiokampus.fm/playlista.php")
+    r = __download_json("https://api.radiokampus.fm/playlista/?day=20200223")
     tracks = __prepare_list_of_tracks(r)
     print(tracks)
